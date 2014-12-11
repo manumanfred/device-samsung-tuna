a94ae556114654c43cdd2cc8024d9c4031330678
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2014-12-11 21:27:54 +0200

    GCC 4.7 for a kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_KERNEL_CONFIG := dreams_tuna_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/tuna
 
 TARGET_PREBUILT_KERNEL := device/samsung/tuna/kernel
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
 
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := omap4
