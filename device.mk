62c4e96e120153ad7c66bdb893d6dc0fea240051
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2015-01-16 22:54:11 +0200

    Force API1 for camera

--- device.mk
+++ device.mk
@@ -179,7 +179,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.com.google.clientidbase=android-google \
 	ro.com.android.wifi-watchlist=GoogleGuest \
 	ro.setupwizard.enterprise_mode=1 \
-	ro.com.android.dateformat=MM-dd-yyyy
+	ro.com.android.dateformat=MM-dd-yyyy \
+	camera2.portability.force_api=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=131072
